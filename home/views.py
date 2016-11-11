11b520e1b66a13709fb074ad03205997ccca04e4
Michael <mikeskolnik75@gmail.com>
2016-11-11 01:33:43 +0000

    Fifty-fourth commit - Feed pagination - First attempt - Not working correctly

--- home/views.py
+++ home/views.py
@@ -9,7 +9,10 @@ from feed.forms import FeedPostForm
 home_app = Blueprint("home_app", __name__)
 
 @home_app.route("/")
-def home():
+@home_app.route("/<int:feed_page_num>/", endpoint="feed-home-page")
+def home(feed_page_num=1):
+    feed_messages_page = False
+    
     if session.get("username"):
         form = FeedPostForm()
     
@@ -17,9 +20,19 @@ def home():
             username=session.get("username")
             ).first()
         
+        feed_messages_total = Feed.objects.filter(
+            user=user
+            ).count()
+        
         feed_messages = Feed.objects.filter(
             user=user
-            ).order_by("-create_date")[:10]
+            ).order_by("-create_date")
+        
+        if feed_messages_total > 10:
+            feed_messages_page = True
+            feed_messages = feed_messages.paginate(page=feed_page_num, per_page=10)
+        else:
+            feed_messages = feed_messages[:10]
         
         return render_template("home/feed_home.html",
                                user=user,
